ab3b31c99b9bf4f3407cb79911a4c3381ab356a4
Leo So <leosohc@gmail.com>
2018-12-11 16:12:30 +0800

    eventlist updated and need to be fixed

--- src/components/eventList.vue
+++ src/components/eventList.vue
@@ -1,9 +1,48 @@
 <template>
-    <p>event</p>
-
-
+    <div id="eventContent" class="container">
+        <div class="page-header">
+            <h1>View the list of the current events</h1>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3>Event List</h3>
+            </div>
+            <div class="panel-body">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th>Datetime</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for"event in {{events}}">
+                            <th>{{event.title}}</th>
+                            <th>{{event.datetime}}</th> 
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </template>
 
-<script></script>
+<script>
+import { db } from "../main";
+
+export default {
+  name: "eventList",
+  data() {
+    return {
+        events: []
+    };
+  },
+  firestore () {
+    return {
+      events: db.collection('event').orderBy('datetime')
+    }
+  }
+};
+</script>
 
 <style></style>
