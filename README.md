f0c061d291dc50be08cea98ee9f9dc13a165a830
Keith <suenkaleongkeith@gmail.com>
2018-12-13 00:58:49 +0800

    update about

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
   
 ## Project Requirement
 
-### User
+### User (So Hon Chuen, Shum Hiu Tung)
 | Requirement | Progress |
 |--|--|
 | 1. SignUp + Login | **Need testing** |
@@ -14,7 +14,7 @@
 | 5. Add event to favourite + see list in another view |  |
 | 6. See user name in top right corner + Logout | **Need testing** |
 
-### Admin
+### Admin (Suen Ka Leong, Shum Hiu Tung)
 | Requirement | Progress |
 |--|--|
 | 1. Flush Data | **Need testing** |
@@ -23,7 +23,7 @@
 | 4. Load CSV | **Need testing** |
 | 5. Logout | **Need testing** |
 
-### Non-user
+### Non-user (Shum Hiu Tung)
 | Requirement | Progress |
 |--|--|
 | 1. Login + SignUp | **Need testing** |
@@ -58,7 +58,7 @@ npm run lint
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
 
-## 2. Node.js Part (server):
+## 2. Node.js Part:
 ## run
 ```
 node server/server.js
@@ -67,3 +67,25 @@ node server/server.js
 ```
 nodemon server/server.js
 ```
+
+
+3. Design of data schemas and models of your database
+
+There are two collections in the database: event, user
+user(String username, String email, Array favEvents)
+event(String title, String datetime, String organization, String venue, String district)
+
+For user password, it is saved in the Authenication of the Firebase. The password is automaticaly hashed by Firebase.
+
+4. Technologies and frameworks/libraries in use
+Node.js Express
+Vue.js Vuetify.js
+	Advantages: - the coding is simplier than Angular
+				- the size is smaller than Angular
+	Disadvantages: - a new frameworks with not much resources in the Internet
+				   - WE DIDN'T LEARN VUE.JS IN CLASS, use more time to learn
+Firebase
+	Advantages: - Authenication of the application can be easily done
+				- Great GUI of the database console
+	Disadvantages: - does not provide high performance like MongoDB
+				   - Firebase Admin SDK only support some programming language, that's why we need an extra Node.js server
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,85 +1,169 @@
 <template>
+    <v-content>
   <v-container>
     
     <v-layout
-      text-xs-left
+      text-xs-left ma-3
       wrap
       pl-5
       pr-5
     >
-      <v-flex xs12>
-  <div class="about">
-    
+      <v-flex xs12 sm12 md12 text-xs-left ma-3>
+            <v-card class="elevation-12">
+    <v-card-text>
     
         <h1># csci2720_project</h1>
+    </v-card-text>
+    </v-card>
+</v-flex>
+<v-flex xs12 sm12 md12 text-xs-left ma-3>
+            <v-card class="elevation-12">
+    <v-card-text>
+    
+        <h3>1. Names and workload distribution of each group member</h3>
+        <table>
+            <thead>
+              <tr>
+                <th>
+                  Names
+                </th>
+                <th>
+                  Workload
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  Shum Hiu Tung
+                </td>
+                <td>
+                  Admin + User + No User
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  So Hon Chuen 
+                </td>
+                <td>
+                  User 
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  Suen Ka Leong
+                </td>
+                <td>
+                  Admin
+                </td>
+              </tr>
+            </tbody>
+          </table>
+    </v-card-text>
+    </v-card>
+</v-flex>
+<v-flex xs12 sm12 md12 text-xs-left ma-3>
+            <v-card class="elevation-12">
+                <v-card-text>
 
-          
-        <h2>## Project Requirement</h2>
-
-        <h3>### User</h3>
-        | Requirement | Progress | <br>
-        |--|--|<br>
-        | 1. SignUp + Login | **Need testing** |<br>
-        | 2. List event in table (allow sorting) | **Need testing** |<br>
-        | 3. Search by one field |  |<br>
-        | 4. Separate view of a event: detail + comment(non-threaded) |  |<br>
-        | 5. Add event to favourite + see list in another view |  |<br>
-        | 6. See user name in top right corner + Logout | **Need testing** |<br>
-        <h3>### Admin</h3>
-        | Requirement | Progress |<br>
-        |--|--|<br>
-        | 1. Flush Data | **Need testing** |<br>
-        | 2. CRUD event data | **Need testing** |<br>
-        | 3. CRUD user data | **Need testing** |<br>
-        | 4. Load CSV | **Need testing** |<br>
-        | 5. Logout | **Need testing** |<br>
-
-        <h3>### Non-user</h3>
-        | Requirement | Progress |<br>
-        |--|--|<br>
-        | 1. Login + SignUp | **Need testing** |<br>
-        | 2. Login as admin | **Need testing** |<br>
-
-
-        <h2>## Project setup</h2>
+        <h3>2. Basic “how-to” of your project app</h3><hr>
+        <h5>## Project setup</h5>
         ```<br>
         npm install<br>
         ```<br>
 
-        <h3>## 1. Vue.js Part:</h3>
+        <h4>## 1. Vue.js Part:</h4>
 
-        <h4>### Compiles and hot-reloads for development</h4>
+        <h5>### Compiles and hot-reloads for development</h5>
         ```<br>
         npm run serve<br>
         ```<br>
-        <h4>### Compiles and minifies for production</h4>
+        <h5>### Compiles and minifies for production</h5>
         ```<br>
         npm run build<br>
         ```<br>
 
-        <h4>### Run your tests</h4>
+        <h5>### Run your tests</h5>
         ```<br>
         npm run test<br>
         ```<br>
-        <h4>### Lints and fixes files</h4>
+        <h5>### Lints and fixes files</h5>
         ```<br>
         npm run lint<br>
         ```<br>
 
-        <h4>### Customize configuration</h4>
+        <h5>### Customize configuration</h5>
         See [Configuration Reference](https://cli.vuejs.org/config/).<br>
 
-        <h3>## 2. Node.js Part (server):</h3>
-        <h4>## run</h4>
+        <h4>## 2. Node.js Part:</h4>
+        <h5>## run</h5>
         ```<br>
         node server/server.js<br>
         ```<br>
-        <h4>## hot-reloads</h4>
+        <h5>## hot-reloads</h5>
         ```<br>
         nodemon server/server.js<br>
         ```<br>
-  </div>
-</v-flex>
+</v-card-text></v-card></v-flex>
+<v-flex xs12 sm12 md12 text-xs-left ma-3>
+            <v-card class="elevation-12">
+                <v-card-text>
+        <h3>3. Design of data schemas and models of your database</h3><hr>
+        There are two collections in the database: event, user<br>
+        user(String username, String email, Array favEvents)<br>
+        event(String title, String datetime, String organization, String venue, String district)<br>
+        For user password, it is saved in the Authenication of the Firebase. The password is automaticaly hashed by Firebase.<br>
+
+</v-card-text></v-card></v-flex>
+<v-flex xs12 sm12 md12 text-xs-left ma-3>
+            <v-card class="elevation-12">
+                <v-card-text>
+        <h3>4. Technologies and frameworks/libraries in use</h3><hr>
+        <h5>Node.js Express</h5>
+        <h5>Vue.js Vuetify.js</h5>
+            Advantages: <br>
+            - the coding is simplier than Angular<br>
+            - the size is smaller than Angular<br>
+            Disadvantages:
+            - a new frameworks with not much resources in the Internet<br>
+            - WE DIDN'T LEARN VUE.JS IN CLASS, use more time to learn<br>
+        <h5>Firebase</h5>
+            Advantages: <br>
+            - Authenication of the application can be easily done<br>
+            - Great GUI of the database console<br>
+            Disadvantages: <br>
+            - does not provide high performance like MongoDB<br>
+            - Firebase Admin SDK only support some programming language, that's why we need an extra Node.js server<br>
+</v-card-text></v-card></v-flex>
+
 </v-layout>
 </v-container>
+</v-content>
 </template>
+<style>
+table {
+  border: 2px solid #000000;
+  border-radius: 3px;
+  background-color: #fff;
+}
+
+th {
+  background-color: #000000;
+  color: rgba(255,255,255,0.66);
+  cursor: pointer;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+td {
+  background-color: #f9f9f9;
+}
+
+th, td {
+  min-width: 120px;
+  padding: 10px 20px;
+}
+
+</style>
