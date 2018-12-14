0d9e75699ef982ba1e8c2d5af5eb4bb8b0a6ba3a
Keith <suenkaleongkeith@gmail.com>
2018-12-14 23:54:06 +0800

    fix fav->eventdatils bug

--- src/components/favouriteList.vue
+++ src/components/favouriteList.vue
@@ -20,7 +20,8 @@
                     <td class="text-xs-left">{{ props.item.datetime }}</td>
                     <td class="justify-center layout pa-3">
                         <!-- <v-icon small class="mr-2" @click="editItem(props.item)">edit</v-icon> -->
-                    <router-link :to="'/events/' + props.item.id +'/'+ props.item.title + '/' + props.item.datetime + '/' + props.item.organization + '/' + props.item.venue + '/' + props.item.district">
+                    <!-- <router-link :to="'/events/' + props.item.id +'/'+ props.item.title + '/' + props.item.datetime + '/' + props.item.organization + '/' + props.item.venue + '/' + props.item.district"> -->
+                        <router-link :to="'/events/' + props.item.id">
                         <v-icon>more_horiz</v-icon>
                     </router-link>
                         <v-icon @click="deleteFav(props.item.id)">delete</v-icon>
